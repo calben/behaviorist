527fcc3b0e67259b7bf9d0847f441d077cce737e
Calem Bendell <calem.j.bendell@gmail.com>
2016-01-19 16:16:04 -0500

    create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='Behaviorist',
+        version='0.1',
+        packages=['behaviorist'],
+        url='https://github.com/calben/behaviorist',
+        license='',
+        author='Calem Bendell',
+        author_email='calem.j.bendell@gmail.com',
+        description=''
+)
